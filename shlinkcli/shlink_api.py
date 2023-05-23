5681b159d9e428340d73fda544c4094cf72e980e
Pawan Rai <pawanrai9999@gmail.com>
2023-05-23 22:33:16 +0530

    Added License Header

--- shlinkcli/shlink_api.py
+++ shlinkcli/shlink_api.py
@@ -1,5 +1,15 @@
-# create a ShlinkApi class which will take requests.Session as a constructor argument, it will make various requests to shlink api
-# and return the response as a json object
+# Copyright (C) 2023  Pawan Rai <pawanrai9999@gmail.com>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from typing import Any
 
 import requests
